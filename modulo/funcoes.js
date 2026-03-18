7b0c10598db31940acac0911b659feacec173ca2
Nephyro <anderson.soare.4245@gmail.com>
2026-03-18 11:18:12 -0300

    enviando arquivos

---
+++ modulo/funcoes.js
@@ -0,0 +1,25 @@
+/* *******************************************************************************
+* Objetivo: Arquivo responsável por manipular dados do arquivo estados_cidades.js
+* Data: 29/08/2022  
+* Autor: Anderson Ribeiro
+* ********************************************************************************/
+
+const getListaDeEstados = function(){
+
+}
+
+const getDadosEstado = function(){
+
+}
+
+const getCapitalEstado = function(){
+
+}
+
+const getEstadosRegiao = function(){
+
+}
+
+const getCapitalPais = function(){
+
+}
